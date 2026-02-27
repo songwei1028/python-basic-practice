b573da510064631a2fc5f0c446255b395f70b8fb
songwei <songw1@chinatelecom.cn>
2026-02-27 17:14:12 +0800

    20260227 practice

---
+++ chapter1/02_string.py
@@ -0,0 +1,23 @@
+# 大小写
+# name = "ada lovelace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+# 拼接
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = first_name + " " + last_name
+#
+# print(full_name)
+
+# 添加空白
+# print("Python")
+# print("\tPython")
+# print("\nPython")
+# print("\n\tPython")
+
+# 删除空白
+# favorite_language = '   python '
+# print(favorite_language)
+# print(favorite_language.rstrip())
---
+++ chapter1/03_number.py
@@ -0,0 +1,8 @@
+# 整数
+
+# 浮点数：带小数点的数字
+
+# 函数str()避免错误类型
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
