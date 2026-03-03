a1e37455e0febca4d25e944b1bc952452dfbe07f
songwei <songw1@chinatelecom.cn>
2026-03-03 16:18:21 +0800

    20260303 practice

---
+++ chapter4/dict_02.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for key, value in favorite_languages.items():
+    print(key + ": " + value + "\n")
+
+keys = favorite_languages.keys()
---
+++ chapter4/dict_03.py
@@ -0,0 +1,9 @@
+# alien_0 = {'color': 'green', 'points': 5}
+# alien_1 = {'color': 'yellow', 'points': 10}
+# alien_2 = {'color': 'red', 'points': 15}
+# aliens = [alien_0, alien_1, alien_2]
+
+aliens =[]
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
